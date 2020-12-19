023ab633bd76a8927051df56753b029b1baaf71f
System Administrator <root@Pavels-MacBook-Pro.local>
2020-12-20 00:40:38 +0200

    Integrated with the Bootstrap starter template

--- notepad/src/notes/views.py
+++ notepad/src/notes/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Note
 
 # Create your views here.
+def note_list_view(request):
+    notes = Note.objects.all()
+    context = {'notes': notes}
+    return render(request, "note_list.html", context)
