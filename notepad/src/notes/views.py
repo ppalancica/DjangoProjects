fd1a0be29dc6483ce7571294e2c0e047c5805e07
System Administrator <root@Pavels-MacBook-Pro.local>
2020-12-20 03:36:23 +0200

    Added a form on the top

--- notepad/src/notes/views.py
+++ notepad/src/notes/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Note
+from .forms import NoteModelForm
 
 # Create your views here.
 def note_list_view(request):
+    form = NoteModelForm()
+    if request.method == "POST":
+        form = NoteModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('note-list')
     to_do_list = Note.objects.filter(finished=False)
     finished_list = Note.objects.filter(finished=True)
     # notes = Note.objects.all()
     # context = {'notes': notes}
     context = {
         'to_do_list': to_do_list,
-        'finished_list': finished_list
+        'finished_list': finished_list,
+        'form': form
     }
     return render(request, "note_list.html", context)
 
