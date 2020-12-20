b92b2167642d2ebc00d54a4cc5f26a0eca7ffd09
System Administrator <root@Pavels-MacBook-Pro.local>
2020-12-20 03:20:23 +0200

    Added URL creation methods inside the Note class, to be used in the note_snippet.html

--- notepad/src/notes/models.py
+++ notepad/src/notes/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.shortcuts import redirect, reverse
 
 # First parameter is what's stored in the database
 LABEL_CHOICES = (
@@ -20,3 +21,18 @@ class Note(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_finish_item_url(self):
+        return reverse("finish-note-item", kwargs={
+            'id': self.id
+        })
+
+    def get_recover_item_url(self):
+        return reverse("recover-note-item", kwargs={
+            'id': self.id
+        })
+
+    def get_delete_item_url(self):
+        return reverse("delete-note-item", kwargs={
+            'id': self.id
+        })
