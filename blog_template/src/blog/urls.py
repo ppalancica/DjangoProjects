4de306d13386c4de0f339e4ea868a830eb8e9e68
System Administrator <root@Pavels-MacBook-Pro.local>
2020-12-23 01:51:53 +0200

    Integrated witht the Bootstrap theme

--- blog_template/src/blog/urls.py
+++ blog_template/src/blog/urls.py
@@ -18,8 +18,13 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
+from .views import index, blog, post
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index),
+    path('blog/', blog),
+    path('post/', post),
 ]
 
 if settings.DEBUG:
