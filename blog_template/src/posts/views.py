8aa073ad98f63f22ec4b9f68f424dcc5fb5d4642
System Administrator <root@Pavels-MacBook-Pro.local>
2020-12-23 06:05:27 +0200

    Added some code for form submission

--- blog_template/src/posts/views.py
+++ blog_template/src/posts/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from .models import Post
+from marketing.models import Signup
 
 def index(request):
     # queryset = Post.objects.filter(featured=True)
     featured = Post.objects.filter(featured=True)
     latest = Post.objects.order_by('-timestamp')[0:3]
+
+    if request.method == "POST":
+        email = request.POST["email"]
+        new_signup = Signup()
+        new_signup.email = email
+        new_signup.save()
+
     context = {
         'object_list': featured,
         'latest': latest
