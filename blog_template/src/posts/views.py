43f54738ff0686398f21cd86d174687d102e7cad
System Administrator <root@Pavels-MacBook-Pro.local>
2020-12-23 15:44:58 +0200

    Added pagination

--- blog_template/src/posts/views.py
+++ blog_template/src/posts/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 from .models import Post
 from marketing.models import Signup
@@ -21,8 +22,21 @@ def index(request):
 
 def blog(request):
     post_list = Post.objects.all()
+    paginator = Paginator(post_list, 2)
+    page_request_var = 'page'
+    page = request.GET.get(page_request_var)
+    try:
+        paginated_queryset = paginator.page(page)
+    except PageNotAnInteger:
+        paginated_queryset = paginator.page(1)
+    except EmptyPage:
+        # If it's an empty page, return the last page
+        paginated_queryset = paginator.page(paginator.num_pages)
+
     context = {
-        'post_list': post_list
+        # 'post_list': post_list,
+        'queryset': paginated_queryset,
+        'page_request_var': page_request_var
     }
     return render(request, 'blog.html', context)
 
